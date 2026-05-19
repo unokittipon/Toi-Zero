9f645156ec34b30c6ed9ed4a9886aa5ba51cfc4e
Kittipon <unokittipon@gmail.com>
2026-05-19 19:12:15 +0700

    “add”

---
+++ A1/A1-019.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define sp << ' ' <<
+#define nl << '\n' 
+#define cnl cout << '\n'
+using namespace std;
+const int nx = 1e5+5;
+const int INF = 1e9+5;
+const int MOD = 1e9+7;
+
+
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    int a,b,c;
+    cin >> a >> b >> c;
+    if(a == b && b == c)cout << "all the same";
+    else if(a != b && b != c && a != c)cout << "all different";
+    else cout << "neither";
+}
